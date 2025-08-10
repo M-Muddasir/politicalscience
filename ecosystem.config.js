f0d701fe1d72dce824c4dd0d1f959814082b65e3
M-Muddasir <chmudasirbwn775@gmail.com>
2025-08-10 23:18:53 +0500

    fixed ecoSystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,9 @@ module.exports = {
       script: "npm",
       args: "start -- --port 3000 --hostname 0.0.0.0",
       env: {
-        DATABASE_URL: "postgresql://politicalscience:secure_password_123@ec2-54-227-218-115.compute-1.amazonaws.com:5432/politicalscience_db"
+        NODE_ENV: 'production',
+        PORT: 3000,
+        DATABASE_URL: 'postgresql://politicalscience:secure_password_123@localhost:5432/politicalscience_db'
       }
     }
   ]
